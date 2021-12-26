651b66e7e1d3a8c0ddaa01dd415a3108350829bb
jparadelo <juniorparadelo@gmail.com>
2021-12-26 20:12:14 +0100

    User controller with moongose

--- backend/src/controllers/users.controller.js
+++ backend/src/controllers/users.controller.js
@@ -1,13 +1,38 @@
 const usersController = {};
 
-usersController.getUsers = (req, res) => res.json({ message: "GET users" });
+const User = require("../models/User");
 
-usersController.createUser = (req, res) => res.json({ message: "CREATE user" });
+usersController.getUsers = async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+};
 
-usersController.getUser = (req, res) => res.json({ message: "GET user" });
+usersController.createUser = async (req, res) => {
+  const { username } = req.body;
+  const newUser = new User({ username });
+  await newUser.save();
+  res.json("User created!");
+};
 
-usersController.updateUser = (req, res) => res.json({ message: "UPDATE user" });
+usersController.getUser = async (req, res) => {
+  const user = await User.findById(req.params.id);
+  res.json(user);
+};
 
-usersController.deleteUser = (req, res) => res.json({ message: "DELETE user" });
+usersController.updateUser = async (req, res) => {
+  const { username } = req.body;
+  await User.findOneAndUpdate(
+    { _id: req.params.id },
+    {
+      username,
+    }
+  );
+  res.json({ message: "User updated!" });
+};
+
+usersController.deleteUser = async (req, res) => {
+  await User.findByIdAndDelete(req.params.id);
+  res.json({ message: "User deleted!" });
+};
 
 module.exports = usersController;
